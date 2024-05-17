a36d979e70e7b94664d470690ca876f906681e5b
Kseniia <ksovechkina@gmail.com>
2024-05-17 21:58:32 +1200

    madules js

---
+++ api.js
@@ -0,0 +1,22 @@
+export function getComments() {
+    return fetch("https://wedev-api.sky.pro/api/v1/kseniia-ovechkina/comments", {
+        method: "GET",
+    }).then((response) => {
+        if (response.status === 200) {
+            return response.json();
+        } else if (response.status === 500) {
+            throw new Error('Bad request');
+        };
+    });
+};
+
+export function postComment({ name, text }) {
+    return fetch("https://wedev-api.sky.pro/api/v1/kseniia-ovechkina/comments", {
+        method: "POST",
+        body: JSON.stringify({
+            name: name,
+            text: text,
+            forceError: true,
+        }),
+    });
+};
